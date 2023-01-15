3fac96eb4b37b64dd0c50ee86e89d8d535a4ce1f
Jaden Hong <jaden@Jadens-MacBook-Air.local>
2023-01-15 18:05:44 -0500

    first initial commit

---
+++ CoffeeTime/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  CoffeeTime
+//
+//  Created by Jaden Hong on 2023-01-08.
+//
+
+import Foundation
+
+struct Constants {
+    static let GoogleMapsAPIKey = "AIzaSyCcE3nu7UOWUrYPzreBsL0JJFiKvWdV4XA"
+    
+    
+}
---
+++ CoffeeTime/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoffeeTime
+//
+//  Created by Jaden Hong on 2023-01-07.
+//
+
+import UIKit
+import GoogleMaps
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
